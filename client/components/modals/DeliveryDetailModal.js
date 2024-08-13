d4a78722e252f15276210198083828c5baa6fc5b
Gavriel Rudolph <gavrielmrudolph@gmail.com>
2024-08-12 20:32:26 -0400

    feat: begin implementing styling and functionality for Deliveries screen and modal

---
+++ client/components/modals/DeliveryDetailModal.js
@@ -0,0 +1,176 @@
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import React from 'react';
+import { Dimensions, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const DeliveryDetailModal = ({ visible, onClose, deliveryDetail }) => {
+    if (!deliveryDetail) return null;
+
+    return (
+        <Modal
+            visible={visible}
+            animationType="slide"
+            transparent={true}
+            onRequestClose={onClose}
+        >
+            <View style={styles.modalContainer}>
+                <View style={styles.outerBorder}>
+                    <View style={styles.modalContent}>
+                        {/* Modal Header with Restaurant Name */}
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.restaurantName}>{deliveryDetail.restaurant_name}</Text>
+                        </View>
+                        {/* Header Info with Order Date, Status, and Customer Info */}
+                        <View style={styles.headerInfoContainer}>
+                            <View style={styles.headerInfo}>
+                                <Text style={styles.orderInfo}>Order Date: {new Date(deliveryDetail.created_at).toLocaleDateString()}</Text>
+                                <Text style={styles.orderInfo}>Status: {deliveryDetail.status.toUpperCase()}</Text>
+                                <Text style={styles.orderInfo}>Customer: {deliveryDetail.customer_name}</Text>
+                                <Text style={styles.orderInfo}>Address: {deliveryDetail.customer_address}</Text>
+                                <Text style={styles.orderInfo}>Phone: {deliveryDetail.customer_phone}</Text>
+                                <Text style={styles.orderInfo}>Email: {deliveryDetail.customer_email}</Text>
+                            </View>
+                            {/* Close Button */}
+                            <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+                                <FontAwesomeIcon icon={faX} size={24} color="grey" />
+                            </TouchableOpacity>
+                        </View>
+                        {/* Content with Ordered Items and Total Cost */}
+                        <View style={styles.contentContainer}>
+                            <View style={styles.itemsContainer}>
+                                {deliveryDetail.products.map(item => (
+                                    <View key={item.product_id} style={styles.itemRow}>
+                                        <Text style={styles.itemName}>{item.product_name}</Text>
+                                        <Text style={styles.itemQuantity}>x{item.quantity}</Text>
+                                        <Text style={styles.itemPrice}>$ {(item.total_cost / 100).toFixed(2)}</Text>
+                                    </View>
+                                ))}
+                            </View>
+                            <View style={styles.separator} />
+                            <Text style={styles.total}>
+                                <Text style={styles.totalLabel}>TOTAL: </Text>
+                                $ {((deliveryDetail.total_cost || 0) / 100).toFixed(2)}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    outerBorder: {
+        width: '90%',
+        borderRadius: 10,
+        backgroundColor: '#FFFFFF',
+        padding: 8, // This will be the white border thickness
+        marginTop: 0.3 * height,
+    },
+    modalContent: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        overflow: 'hidden',
+        borderWidth: 2,
+        borderColor: '#EEEEEE', // Thin grey border
+    },
+    modalHeader: {
+        backgroundColor: '#222126',
+        paddingHorizontal: 20,
+        paddingTop: 15,
+        paddingBottom: 5,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+    },
+    restaurantName: {
+        fontSize: 28,
+        color: '#E95420',
+        fontWeight: 'bold',
+        fontFamily: 'Oswald-Bold',
+        marginLeft: 10,
+    },
+    headerInfoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start', // Align items to the start (top)
+        backgroundColor: '#222126',
+        paddingHorizontal: 20,
+        paddingTop: 5,
+        paddingBottom: 15,
+    },
+    headerInfo: {
+        flex: 1,
+        marginLeft: 10,
+        marginBottom: 5,
+    },
+    orderInfo: {
+        fontSize: 16,
+        color: '#FFFFFF',
+        fontFamily: 'Arial',
+        marginVertical: 2,
+    },
+    closeButton: {
+        paddingTop: 0, // Adjust padding to align with text
+    },
+    contentContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    itemsContainer: {
+        marginVertical: 10,
+    },
+    itemRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        paddingVertical: 5,
+    },
+    itemName: {
+        fontSize: 16,
+        color: '#222126',
+        fontFamily: 'Arial',
+        flex: 3,
+    },
+    itemQuantity: {
+        fontSize: 18,
+        color: '#222126',
+        fontFamily: 'Arial',
+        textAlign: 'left',
+        flex: 1,
+    },
+    itemPrice: {
+        fontSize: 18,
+        color: '#222126',
+        fontFamily: 'Arial',
+        textAlign: 'right',
+        flex: 1,
+    },
+    separator: {
+        borderBottomColor: '#000000',
+        borderBottomWidth: 1,
+        marginVertical: 0,
+    },
+    total: {
+        fontSize: 18,
+        color: '#222126',
+        fontFamily: 'Oswald-Medium',
+        textAlign: 'right',
+        marginTop: 2,
+        marginBottom: 12,
+    },
+    totalLabel: {
+        fontWeight: 'bold',
+        fontFamily: 'Oswald-Bold',
+    },
+});
+
+export default DeliveryDetailModal;
--- client/components/screens/DeliveriesScreen.js
+++ client/components/screens/DeliveriesScreen.js
@@ -1,10 +1,13 @@
-// DeliveriesScreen.js
-
+import { faMagnifyingGlassPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React, { useEffect, useState } from 'react';
-import { Alert, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Alert, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import DeliveryDetailModal from '../modals/DeliveryDetailModal'; // Assume this modal is similar to OrderHistoryDetailModal
 
 const DeliveriesScreen = () => {
     const [deliveries, setDeliveries] = useState([]);
+    const [selectedDelivery, setSelectedDelivery] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
 
     useEffect(() => {
         fetchDeliveries();
@@ -12,7 +15,7 @@ const DeliveriesScreen = () => {
 
     const fetchDeliveries = async () => {
         try {
-            const response = await fetch(`${process.env.EXPO_PUBLIC_URL}/api/deliveries`);
+            const response = await fetch(`${process.env.EXPO_PUBLIC_URL}/api/orders?id=1&type=courier`); // Adjusted for courier type
             const data = await response.json();
             setDeliveries(data);
         } catch (error) {
@@ -21,20 +24,88 @@ const DeliveriesScreen = () => {
         }
     };
 
+    const handleDeliveryPress = (delivery) => {
+        setSelectedDelivery(delivery);
+        setModalVisible(true);
+    };
+
+    const handleStatusChange = async (delivery) => {
+        const nextStatus = delivery.status === 'pending' ? 'in progress' : delivery.status === 'in progress' ? 'delivered' : delivery.status;
+
+        try {
+            const response = await fetch(`${process.env.EXPO_PUBLIC_URL}/api/orders/${delivery.id}/status`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ status: nextStatus }),
+            });
+
+            if (response.ok) {
+                fetchDeliveries(); // Refresh the deliveries list after updating status
+            } else {
+                Alert.alert('Error', 'Failed to update the status. Please try again.');
+            }
+        } catch (error) {
+            console.error('Error updating status:', error);
+            Alert.alert('Error', 'Unable to update status. Please try again later.');
+        }
+    };
+
+    // Function to extract building number and street from the full address
+    const formatAddress = (address) => {
+        const parts = address.split(',');
+        return parts[0]; // Return only the building number and street
+    };
+
+    const renderDelivery = ({ item }) => (
+        <View style={styles.tableRow}>
+            <Text style={styles.tableCellId}>{item.id}</Text>
+            <Text style={styles.tableCellAddress}>{formatAddress(item.customer_address)}</Text>
+            <TouchableOpacity
+                style={[
+                    styles.tableCellStatus,
+                    item.status === 'pending' ? styles.statusPending :
+                    item.status === 'in progress' ? styles.statusInProgress :
+                    styles.statusDelivered,
+                ]}
+                onPress={() => handleStatusChange(item)}
+            >
+                <Text style={styles.statusText}>{item.status.toUpperCase()}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => handleDeliveryPress(item)}
+                style={styles.tableCellView}
+            >
+                <FontAwesomeIcon icon={faMagnifyingGlassPlus} size={20} color="#222126" />
+            </TouchableOpacity>
+        </View>
+    );
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Deliveries</Text>
-            <FlatList
-                data={deliveries}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => (
-                    <View style={styles.delivery}>
-                        <Text style={styles.deliveryText}>Order ID: {item.order_id}</Text>
-                        <Text style={styles.deliveryText}>Courier ID: {item.courier_id}</Text>
-                        <Text style={styles.deliveryText}>Status: {item.status}</Text>
-                    </View>
-                )}
-            />
+            <Text style={styles.pageTitle}>MY DELIVERIES</Text>
+            <View style={styles.table}>
+                <View style={styles.tableHeader}>
+                    <Text style={styles.tableHeaderTextId}>ORDER ID</Text>
+                    <Text style={styles.tableHeaderTextAddress}>ADDRESS</Text>
+                    <Text style={styles.tableHeaderTextStatus}>STATUS</Text>
+                    <Text style={styles.tableHeaderTextView}>VIEW</Text>
+                </View>
+                <FlatList
+                    data={deliveries}
+                    renderItem={renderDelivery}
+                    keyExtractor={item => item.id.toString()}
+                    style={styles.scrollableList} // Added this style to control height and scroll
+                />
+            </View>
+            {selectedDelivery && (
+                <DeliveryDetailModal
+                    visible={modalVisible}
+                    onClose={() => setModalVisible(false)}
+                    deliveryDetail={selectedDelivery}
+                />
+            )}
         </View>
     );
 }
@@ -42,24 +113,121 @@ const DeliveriesScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFFFFF',
-        padding: 20,
+        backgroundColor: '#FFFFFF', // Background color
+        paddingHorizontal: 20,
     },
-    title: {
-        fontSize: 24,
+    pageTitle: {
+        fontSize: 24, 
+        color: '#222126', // Text color
         fontWeight: 'bold',
-        color: '#222126',
+        fontFamily: 'Oswald-Regular',
+        marginTop: 30,
         marginBottom: 20,
+        marginLeft: 10,
     },
-    delivery: {
-        backgroundColor: '#F0F0F0',
+    table: {
+        width: '100%',
+        // borderRadius: 8, 
+        overflow: 'hidden',
+        flex: 1,
+        
+    },
+    tableHeader: {
+        flexDirection: 'row',
+        backgroundColor: '#222126', // Header background color
+        padding: 8,
+        // borderTopLeftRadius: 8,
+        // borderTopRightRadius: 8,
+        alignItems: 'center', // Align items to center
+    },
+    tableHeaderTextId: {
+        flex: 0.5,
+        fontSize: 12,
+        color: '#FFFFFF', // Text color
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        textAlign: 'center', // Center align text
+    },
+    tableHeaderTextAddress: {
+        flex: 1,
+        fontSize: 12,
+        color: '#FFFFFF', // Text color
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        textAlign: 'center', // Center align text
+    },
+    tableHeaderTextStatus: {
+        flex: 2,
+        fontSize: 12,
+        color: '#FFFFFF', // Text color
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        textAlign: 'center', // Center align text
+    },
+    tableHeaderTextView: {
+        flex: 0.3,
+        fontSize: 12,
+        color: '#FFFFFF', // Text color
+        fontWeight: 'bold',
+        fontFamily: 'Arial',
+        textAlign: 'center', // Center align text
+        minWidth: 50,
+    },
+    tableRow: {
+        flexDirection: 'row',
+        backgroundColor: '#FFFFFF', 
         padding: 10,
-        marginBottom: 10,
-        borderRadius: 5,
+        alignItems: 'center', // Align items to center
     },
-    deliveryText: {
-        fontSize: 16,
-        color: '#222126',
+    tableCellId: {
+        flex: 0.5,
+        fontSize: 14, 
+        color: '#222126', // Text color
+        fontFamily: 'Arial',
+        textAlign: 'center', // Center align text
+    },
+    tableCellAddress: {
+        flex: 1.3,
+        fontSize: 14, 
+        color: '#222126', // Text color
+        fontFamily: 'Arial',
+        textAlign: 'left', // Left align text
+        marginLeft: 20,
+        marginRight: 10
+    },
+    tableCellStatus: {
+        flex: 1.3,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        paddingVertical: 5,
+        minWidth: 40,
+        
+    },
+    statusPending: {
+        backgroundColor: '#851919', // Pending status color
+    },
+    statusInProgress: {
+        backgroundColor: '#DA583B', // In Progress status color
+    },
+    statusDelivered: {
+        backgroundColor: '#609475', // Delivered status color
+    },
+    statusText: {
+        fontSize: 14,
+        color: '#FFFFFF', // Text color
+        fontFamily: 'Oswald-Regular',
+        textAlign: 'center', // Center align text
+    },
+    tableCellView: {
+        flex: 0.05,
+        justifyContent: 'center',
+        alignItems: 'center',
+        minWidth: 50,
+        textAlign: 'center', // Center align text
+    },
+    scrollableList: {
+        maxHeight: 400,
     },
 });
 
